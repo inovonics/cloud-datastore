bece13513f08e7cd591575a11a67ed9d6d340bc6
Daniel Williams <dwilliams@inovonics.com>
2017-10-18 14:49:23 -0600

    Filled in the setup script and added a version number.

--- inovonics/cloud/datastore/__init__.py
+++ inovonics/cloud/datastore/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # === IMPORTS ===
+from inovonics.cloud.datastore.__version__ import __version__
 
 # === GLOBALS ===
 
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # === IMPORTS ===
+from distutils.core import setup
 
 # === GLOBALS ===
 
@@ -9,3 +10,12 @@
 # === CLASSES ===
 
 # === MAIN ===
+setup(
+    name='inovonics_cloud_datastore',
+    version='0.1.0.0',
+    description='Basic classes for building a datastore using Redis as a backend.',
+    author='Daniel Williams',
+    author_email='dwilliams@inovonics.com',
+    license='MIT',
+    packages=['inovonics.cloud.datastore']
+)
