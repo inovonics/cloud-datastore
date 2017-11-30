d23c696a1e62f70e06a24b1f2583fb8622787daa
Daniel Williams <dwilliams@inovonics.com>
2017-11-30 10:46:11 -0700

    Trying to make the field types definable.

--- inovonics/cloud/datastore/bases.py
+++ inovonics/cloud/datastore/bases.py
@@ -18,18 +18,37 @@ class InoModelBase:
 
 # FIXME: Add an object base class here
 class InoObjectBase:
-    # Override fields and hidden_fields to give objects attributes
-    # 'oid' is the object's unique identifier.  This prevents collisions with the id() method.
-    fields = ['oid']
-    hidden_fields = []
-    custom_fields = []
+    # Override fields to give objects attributes
+    # Each field should be specified in the 'fields' list by a dictionary containing two entries: 'name' and 'type'.
+    # - 'name' can be a string and will be used as the label in the database.  This should match the names/labels used 
+    # in the DB* objects.
+    # - 'type' should be one of the following: 'bool', 'datetime', 'int', 'str', 'uuid'
+    
+    # A field with name 'oid' is the object's unique identifier.  This had be named to prevents collisions with the id()
+    # method.
+    fields = [{'name': 'oid', 'type': 'uuid'}]
+    custom_fields = [] # All custom fields are strings.
+    
+    allowed_types = ['bool', 'datetime', 'float', 'int', 'str', 'uuid']
     
     def __init__(self, dictionary=None):
         self.logger = logging.getLogger(type(self).__name__)
         # Setup all of the other attributes so they can be written directly
-        for field in self.fields + self.hidden_fields:
-            setattr(self, field, '')
-        setattr(self, 'oid', str(uuid.uuid4()))
+        for field in self.fields:
+            if field['type'] == 'bool':
+                setattr(self, field['name'], False)
+            elif field['type'] == 'datetime':
+                setattr(self, field['name'], datetime.datetime.utcnow())
+            elif field['type'] == 'float':
+                setattr(self, field['name'], 0.0)
+            elif field['type'] == 'int':
+                setattr(self, field['name'], 0)
+            elif field['type'] == 'str':
+                setattr(self, field['name'], '')
+            elif field['type'] == 'uuid':
+                setattr(self, field['name'], uuid.uuid4())
+            else:
+                raise TypeError
         if dictionary:
             self.set_fields(dictionary)
 
@@ -39,14 +58,14 @@ class InoObjectBase:
     def get_dict(self):
         # Get all fields in the object as a dict (excluding hidden fields)
         dictionary = {}
-        for field in self.fields + self.custom_fields:
-            dictionary[field] = getattr(self, field)
-        return dictionary
-
-    def get_all_dict(self):
-        # Get all fields in the object as a dict
-        dictionary = {}
-        for field in self.fields + self.custom_fields + self.hidden_fields:
+        for field in self.fields:
+            if field['type'] == 'datetime':
+                dictionary[field['name']] = getattr(self, field['name']).isoformat()
+            elif field['type'] == 'uuid':
+                dictionary[field['name']] = str(getattr(self, field['name']))
+            else:
+                dictionary[field['name']] = getattr(self, field['name'])
+        for field in self.custom_fields:
             dictionary[field] = getattr(self, field)
         return dictionary
 
@@ -54,7 +73,15 @@ class InoObjectBase:
         if not dictionary:
             return self._validate_fields()
         for field in dictionary:
-            if field in self.fields + self.custom_fields + self.hidden_fields:
+            if field in [i['name'] for i in self.fields]:
+                field_entry = [i for i in self.fields if i['name'] == field][0]
+                if field_entry['type'] == 'datetime':
+                    setattr(self, field_entry['name'], dateutil.parser.parse(dictionary[field]))
+                elif field_entry['type'] == 'uuid':
+                    setattr(self, field_entry['name'], uuid.UUID(dictionary[field]))
+                else:
+                    setattr(self, field_entry['name'], dictionary[field])
+            elif field in self.custom_fields:
                 setattr(self, field, dictionary[field])
             elif field.startswith('custom_'):
                 self.custom_fields.append(field)
